244321883523ca3d3a0b56420ddc120636561ea7
Jonas <jonas@kippingmonkey.se>
2022-04-06 16:49:34 +0200

    add form to index.html. add sendFile to calculator.js

--- calculator.js
+++ calculator.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
 
+//see the "my-express-server" project for notes about basic express use.
+
+// app.get('/', (req, res) => {
+//   res.send("<h1>Hello World!!</h1>")
+// });
+
 app.get('/', (req, res) => {
-  res.send("<h1>Hello World, Wazz up!!</h1>")
-});
+  res.sendFile(__dirname + "/index.html");
+})
 
 app.listen('3000', () => {
   console.log("Server is up and running on port 3000")
